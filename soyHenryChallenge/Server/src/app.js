527e5943d648b32ff229ed389ae46249068b024e
solbenevent <solbenevent6gmail.com>
2023-09-08 12:23:15 -0300

    more changes

--- soyHenryChallenge/Server/src/app.js
+++ soyHenryChallenge/Server/src/app.js
@@ -14,9 +14,9 @@ server.use(express.json());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
     //res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', process.env.CORS_ALLOW_ORIGIN || '*')
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', process.env.CORS_ALLOW_HEADERS || 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 })
