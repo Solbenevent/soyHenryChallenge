114bf2c51fc5e14411224d8e83a0cf37d1ef2be3
solbenevent <solbenevent6gmail.com>
2023-09-12 09:17:27 -0300

    last details

--- soyHenryChallenge/Client/src/components/NavBar.jsx
+++ soyHenryChallenge/Client/src/components/NavBar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { FcSurvey } from "react-icons/fc";
-import "../components/NavBar.css";
+import "../components/Styles/NavBar.css";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid justify-content-center custom-navbar">
         {" "}
-        {/* Añade la clase 'justify-content-center' para centrar el contenido */}
         <a className="navbar-brand" href="#">
           <FcSurvey size={40} /> SURVEY
         </a>
